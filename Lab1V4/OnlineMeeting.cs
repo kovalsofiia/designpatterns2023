7be1c22e05a86145ce0204d8b0777a5c2f021c02
kovalsofiia <koval.sofiia@student.uzhnu.edu.ua>
2023-10-08 11:11:26 +0300

    init

---
+++ Lab1V4/OnlineMeeting.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab1V4
+{
+    internal class OnlineMeeting
+    {
+        //Розробити клас підсистеми для завантаження даних про онлайн зустріч(дата, опис, URL, список учасників)
+        public DateTime date;
+        public string description;
+        public string URl;
+        public SystemUsersList systemUsersList;
+
+        public override string ToString()
+        {
+            string result = $"Date: {date}, Description: {description}, URL: {URl}, List of users: {systemUsersList.ToString()}";
+            return result;
+        }
+
+    }
+}
---
+++ Lab1V4/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab1V4
+{
+    internal class Program
+    {
+        //Варіант 4. Розробити клас підсистеми для завантаження даних про онлайн зустріч(дата, опис, URL, список учасників) та списку всіх користувачів системи(ID, Ім'я, аватар) з файлів в форматі JSON та XML.
+        //Визначення типу файлу визначається при запуску системи.
+        //Передбачити, що дані про кожну зустріч та кожного користувача зберігаються в окремих файлах.
+        //Файл зустрічі містить імена файлів - учасників цієї зустрічі.
+        static void Main(string[] args)
+        {
+        }
+    }
+}
