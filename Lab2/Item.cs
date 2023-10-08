d67fa421bf0860fbc464f9e401a8caa60c8c2949
kovalsofiia <koval.sofiia@student.uzhnu.edu.ua>
2023-10-08 11:32:50 +0300

    lab 2

---
+++ Lab2/Item.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab2
+{
+    internal class Item
+    {
+        //Варіант 1. Дано покупка(Назва товару, Категорія, Виробник, Ціна, Опис, Час покупки, Спосіб оплати, Покупець).
+        public string ItemName { get; set; }
+        public string ItemCategory { get; set; }
+        public string ItemProducer { get; set; }
+        public double ItemPrice { get; set; }
+        public string ItemDescription { get; set; }
+        public DateTime ItemSellTime { get; set; }
+        public string ItemPaymentMethod { get; set; }
+        public string ItemBuyer { get; set; }
+    }
+}
