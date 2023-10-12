93ce9e165e63c5136a04159952337e75cacb3645
kovalsofiia <koval.sofiia@student.uzhnu.edu.ua>
2023-10-12 09:55:54 +0300

    lab2

--- Lab2/Program.cs
+++ Lab2/Program.cs
@@ -52,41 +52,5 @@ namespace Lab2
             Console.WriteLine($"Original Price: ${purchase.Price}");
             Console.WriteLine($"Final Price: ${finalPrice}");
         }
-
-
-        //static List<Item> GenerateUniqueItems()
-        //{
-        //    Console.WriteLine("How many items you want to generate?");
-        //    int n = Convert.ToInt32(Console.ReadLine());
-
-        //    List<Item> items = new List<Item>();
-
-        //    Random random = new Random();
-
-        //    string[] itemNames = { "Ruler", "Pen", "Notebook", "Calculator", "Backpack" };
-        //    string[] categories = { "Office Supplies", "Stationery" };
-        //    string[] brands = { "Bic", "Pilot", "IKEA" };
-        //    string[] paymentMethods = { "Card", "Cash", "Online Payment" };
-        //    string[] buyers = { "buyer1", "buyer2", "buyer3" };
-
-        //    for (int i = 0; i < n; i++)
-        //    {
-        //        string itemName = itemNames[i];
-        //        string itemCategory = categories[random.Next(categories.Length)];
-        //        string itemProducer = brands[random.Next(brands.Length)];
-        //        int itemPrice = random.Next(10, 999);
-        //        string itemDescription = $"Description for {itemName}";
-        //        DateTime itemSellTime = DateTime.Now.AddDays(-random.Next(1, 365));
-        //        string itemPaymentMethod = paymentMethods[random.Next(paymentMethods.Length)];
-        //        string itemBuyer = buyers[random.Next(buyers.Length)];
-
-        //        Item item = new Item(itemName, itemCategory, itemProducer, itemPrice, itemDescription, itemSellTime, itemPaymentMethod, itemBuyer);
-        //        items.Add(item);
-        //        Console.WriteLine($"\n\n{item.ToString()}");
-        //    }
-
-        //    return items;
-
-        //}
     }
 }
